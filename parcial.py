51babaa26da046e1ef2c2b26dd14e7ccccdfa05a
leodan00 <151984036+leodan00@users.noreply.github.com>
2023-11-26 13:40:13 -0500

    p

--- parcial.py
+++ parcial.py
@@ -5,19 +5,20 @@ app = Flask(__name__)
 
 
 
-# Datos de ejemplo (simulando una base de datos)
+
+
 usuarios = [
     {'id': 1, 'nombre': 'Usuario1', 'edad': 25},
     {'id': 2, 'nombre': 'Usuario2', 'edad': 30},
     {'id': 3, 'nombre': 'Usuario3', 'edad': 27}
 ]
 
-# Ruta para obtener todos los usuarios
+
 @app.route('/', methods=['GET'])
 def obtener_usuarios():
     return jsonify({'usuarios': usuarios})
 
-# Ruta para obtener un usuario por su ID
+
 @app.route('/usuario/<int:user_id>', methods=['GET'])
 def obtener_usuario(user_id):
     usuario = next((usuario for usuario in usuarios if usuario['id'] == user_id), None)
@@ -25,7 +26,7 @@ def obtener_usuario(user_id):
         return jsonify({'usuario': usuario})
     return jsonify({'mensaje': 'Usuario no encontrado'}), 404
 
-# Ruta para agregar un nuevo usuario
+
 @app.route('/usuario', methods=['POST'])
 def agregar_usuario():
     nuevo_usuario = request.json
@@ -34,27 +35,6 @@ def agregar_usuario():
     
     usuarios.append(nuevo_usuario)
     return jsonify({'mensaje': 'Usuario agregado correctamente'})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
